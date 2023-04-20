82affbb6a9f1e79b2563b33cb89a8623b43a17ad
KubraAkkaya <akkaya32kubra@gmail.com>
2023-04-20 03:00:46 +0300

    Add project files.

---
+++ EntityLayer/Entities/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Ad")]
+        [StringLength(20, ErrorMessage = "Maximum 20 karakter içermelidir!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Açıklama")]
+        [StringLength(50, ErrorMessage = "Maximum 50 karakter içermelidir!")]
+        public string Decription { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Fiyat")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Stok")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Popüler")]
+        public bool Popular { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Onay")]
+        public bool IsApproved { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Resim")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Adet")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; } //her ürünün 1 Kategorisi olacak.
+    }
+}
---
+++ EntityLayer/Entities/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Ad")]
+        [StringLength(20, ErrorMessage = "Maximum 20 karakter içermelidir!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Soy ad")]
+        [StringLength(20, ErrorMessage = "Maximum 20 karakter içermelidir!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Mail")]
+        [StringLength(50, ErrorMessage = "Maximum 50 karakter içermelidir!")]
+        [EmailAddress (ErrorMessage = "E-mail formatı şeklinde girmelisiniz!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Kullanıcı Ad")]
+        [StringLength(20, ErrorMessage = "Maximum 20 karakter içermelidir!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Şifre")]
+        [StringLength(10, ErrorMessage = "Maximum 10 karakter içermelidir!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez!")] //validaiton
+        [Display(Name = "Şifre")]
+        [StringLength(10, ErrorMessage = "Maximum 10 karakter içermelidir!")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor!")]
+        public string RePassword { get; set; }
+
+        [StringLength(10, ErrorMessage = "Maximum 10 karakter içermelidir!")]
+
+        public string Role { get; set; }
+    }
+}
